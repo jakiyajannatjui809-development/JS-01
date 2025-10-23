1c0c169090aa55c145bfdd2bfe46da8dd5a81d24
jakiyajannatjui809 <jakiyajannatjui809@gmail.com>
2025-10-23 10:20:57 +0600

    Created two index.html for diffrent folder and one js file

---
+++ js/index.js
@@ -0,0 +1,22 @@
+//          
+
+let studentId = 123;
+let studentCgpa = 3.33;
+let studentName = 'Jannat Jui';
+
+// print
+document.writeln ('Student Id:'+studentId+'<br>');
+document.writeln ('Student Cgpa:'+studentCgpa+'<br>');
+document.writeln ('Student Name:'+studentName+'<br>');
+
+// console print
+console.log('Student Id:'+studentId);
+console.log('Student Cgpa:'+studentCgpa);
+console.log('Student Name:'+studentName);
+
+document.writeln('<p style = "colour:red"> '+JannatJui+' </p>');
+
+let cgpa = parseInt(studentCgpa);
+document.writeln('Integer of Float:'+typeof(cgpa));
+document.writeln(cgpa)
+
